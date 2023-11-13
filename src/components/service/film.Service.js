1a93f145600c3f472b14c15c9bd53c184ed0ebfc
Nikita <Hellreg124@gmail.com>
2023-11-13 07:33:12 +0300

    nows

--- src/components/service/film.Service.js
+++ src/components/service/film.Service.js
@@ -3,10 +3,14 @@ import axios from "axios";
 const api_key = '65f680c3-161e-4706-8c05-dc556e90d4e8';
 const api_key_filmID = 'd274e56f-f086-481e-a722-29f85fa01baf';
 const api__poster = '074dcdf3-a07d-4846-bc97-01580169ed46';
-const api__budget = 'c75d1e4d-95e5-4645-b559-d15c7f3cdccd8';
+const api__budget = '10dda4df-4afc-44f6-b863-da996a836f25';
 const api__video = '155416e2-c185-471b-bfac-31b307eceb29';
 const api__simiFilms = '2405b419-17bc-4d42-8bbe-7ce7787a6296';
 const api_search = '9abd2e64-5765-4d27-a4c8-3511edefda07';
+
+//
+
+//c75d1e4d-95e5-4645-b559-d15c7f3cdccd8
  
 export const FilmService = {
 
